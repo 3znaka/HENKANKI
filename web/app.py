12d6d6342b9137c86857170db249a51ed84a4241
3znaka <mightygyvermac@gmail.com>
2024-09-03 18:02:32 +0300

    web  2.0

--- web/app.py
+++ web/app.py
@@ -32,7 +32,7 @@ def remove_header(chunk_with_header):
     file_ext = chunk_with_header[file_ext_offset:file_ext_offset+file_ext_size].decode('ascii')
     chunk_offset = file_ext_offset + file_ext_size
     chunk = chunk_with_header[chunk_offset:]
-    
+
     return chunk, chunk_number, total_chunks, file_name.strip(), file_ext.strip()
 
 def read_qr_codes_from_image(image):
@@ -42,17 +42,17 @@ def read_qr_codes_from_image(image):
 def merge_audio_files():
     files = [f for f in os.listdir(TO_MERGE_FOLDER) if f.endswith('.m4a')]
     file_groups = defaultdict(list)
-    
+
     for file in files:
         prefix = file[:2]
         file_groups[prefix].append(file)
 
     for prefix, group_files in file_groups.items():
         group_files.sort(key=lambda x: int(x[2:6]))
-        
+
         combined = AudioSegment.empty()
         crossfade_duration = 240
-        
+
         for i, file in enumerate(group_files):
             file_path = os.path.join(TO_MERGE_FOLDER, file)
             audio = AudioSegment.from_file(file_path, format='m4a')
@@ -64,16 +64,22 @@ def merge_audio_files():
         temp_output_file = f"{prefix}_temp.mp4"
         temp_output_path = os.path.join(TO_MERGE_FOLDER, temp_output_file)
         combined.export(temp_output_path, format='mp4')
-        
+
         final_output_file = f"{prefix}.aac"
         final_output_path = os.path.join(TO_MERGE_FOLDER, final_output_file)
-        os.system(f"ffmpeg -i {temp_output_path} -acodec aac {final_output_path}")
+
+        # Удаление существующего файла перед началом преобразования
+        if os.path.exists(final_output_path):
+            os.remove(final_output_path)
+
+        # Добавление опции -y для ffmpeg
+        os.system(f"ffmpeg -y -i {temp_output_path} -acodec aac {final_output_path}")
 
         os.remove(temp_output_path)
 
-        for file in group_files:
-            os.remove(os.path.join(TO_MERGE_FOLDER, file))
-    
+    for file in group_files:
+        os.remove(os.path.join(TO_MERGE_FOLDER, file))
+
     return final_output_path
 
 @app.route('/')
@@ -86,6 +92,9 @@ def upload_files():
         return jsonify(success=False, error='Нет файлов для загрузки.')
 
     files = request.files.getlist('files')
+    total_files = len(files)
+    processed_files = 0
+
     files_data = defaultdict(lambda: {
         "decoded_chunks": defaultdict(lambda: None),
         "total_chunks": None,
@@ -106,7 +115,7 @@ def upload_files():
                 qr_data = fix_base64_padding(qr_data)
                 chunk_with_header = base64.b64decode(qr_data)
                 chunk, chunk_number, total_chunks, file_name, file_ext = remove_header(chunk_with_header)
-                
+
                 file_key = (file_name, file_ext)
                 if files_data[file_key]["total_chunks"] is None:
                     files_data[file_key]["total_chunks"] = total_chunks
@@ -119,20 +128,28 @@ def upload_files():
             except (binascii.Error, struct.error) as e:
                 return jsonify(success=False, error=f"Ошибка при обработке QR-кода: {e}")
 
+        processed_files += 1
+        print(f"Обработано страниц: {processed_files} из {total_files}")
+
     for file_key, data in files_data.items():
         if data["total_chunks"] is not None and all(data["decoded_chunks"][i] is not None for i in range(data["total_chunks"])):
             sorted_chunks = [data["decoded_chunks"][i] for i in range(data["total_chunks"])]
             output_file_name = f"{data['file_name']}.{data['file_ext']}"
             output_file_path = os.path.join(TO_MERGE_FOLDER, output_file_name)
+
+            # Проверка и перезапись файла с таким же именем
+            if os.path.exists(output_file_path):
+                os.remove(output_file_path)
+
             try:
                 with open(output_file_path, 'wb') as output_file:
                     for chunk in sorted_chunks:
                         output_file.write(chunk)
             except Exception as e:
                 return jsonify(success=False, error=f"Ошибка при записи файла: {e}")
-    
+
     final_output_file = merge_audio_files()
-    return jsonify(success=True, download_url=f'/download/{os.path.basename(final_output_file)}')
+    return jsonify(success=True, message=f"Файлы успешно загружены и обработаны. Обработано страниц: {processed_files} из {total_files}", download_url=f'/download/{os.path.basename(final_output_file)}')
 
 @app.route('/download/<path:filename>')
 def download_file(filename):
